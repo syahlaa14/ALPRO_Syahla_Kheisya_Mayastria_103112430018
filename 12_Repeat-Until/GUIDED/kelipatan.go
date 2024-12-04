aa21ba3ba54d8b1ce9a8c8a10aa7cc854594ea07
syahlaa14 <108814165+syahlaa14@users.noreply.github.com>
2024-12-04 08:42:08 +0700

    Add files via upload

---
+++ 12_Repeat-Until/GUIDED/kelipatan.go
@@ -0,0 +1,15 @@
+package main  
+import "fmt"  
+
+func main() { 
+    var x, y int
+    var selesai bool 
+
+    fmt.Scan(&x, &y) 
+    for selesai = false; !selesai; { 
+        x = x - y 
+        fmt.Println(x) 
+    	selesai = x <= 0 
+    } 
+    fmt.Println(x == 0) 
+}
